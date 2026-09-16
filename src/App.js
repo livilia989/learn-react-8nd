65ac352eb7be384e82a0f9c0844e45d83a9d01e2
livili989 <livili989@gmail.com>
2026-09-16 13:21:48 +0900

    컴포넌트 실습

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import Greeting from "./Greeting";
+
 function App() {
+  //logic
+  //구조분해할당 - Object destructuring
+  const animals = ["dog","cat","pig"];
+  //const second = animals[1];
+
+  const [ first, second, third ] = animals;
+  console.log("🚀 ~ App ~ first:", first, " / second:", second, " / third:", third);
+
+  const todoItem = {
+    todo: "React학습하기",
+    isComlete: false
+  }
+
+
+  const {todo: todoName} = todoItem
+  console.log("🚀 ~ App ~ todoName:", todoName)
+
+
+
+
+
+  //view
+        /* Greeting.jsx 만든걸 가져다 쓸 수 있음 */
   return (
     <div className="App">
-      변경~~!
+      변경~~! 
+      <Greeting />
+      <Greeting />
+      <Greeting />
+      <Greeting />
     </div>
   );
 }
